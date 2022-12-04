18086eb2ce01b2698e83f5c09d7d9a14e56298fc
Louis <ironarrow18@gmail.com>
2022-12-04 14:23:38 -0500

    Exercise 5
    Created exercise 5: upgraded battle scene with animations and 4 abilities that play different animations when used

--- Assets/Scripts/BattleController.cs
+++ Assets/Scripts/BattleController.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleController : MonoBehaviour
 {
     [SerializeField]
     Canvas battleScreen;
 
+    [SerializeField]
+    Image playerImage;
+
+    [SerializeField]
+    Image enemyImage;
+
+    [SerializeField]
+    Animator playerAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-        battleScreen.gameObject.SetActive(false);
+        //battleScreen.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -19,15 +29,47 @@ public class BattleController : MonoBehaviour
         
     }
 
+    public void AttackButtonClicked()
+    {
+        playerAnimator.Play("PlayerAttack1");
+    }
+
+    public void SpecialAttackButtonClicked()
+    {
+        playerAnimator.Play("PlayerAttack2");
+    }
+
+    public void DefendButtonClicked()
+    {
+        playerAnimator.Play("PlayerGuard");
+    }
+
     public void FleeButtonClicked()
     {
-        //do any required cleanup, then close screen
-        battleScreen.gameObject.SetActive(false);
+        playerAnimator.Play("PlayerRun");
+        playerImage.transform.Rotate(0, 180, 0);
+        StartCoroutine(Flee());
     }
 
     public void BattleTriggered()
     {
         Debug.Log("Battle time!");
         battleScreen.gameObject.SetActive(true);
+    }
+
+    IEnumerator Flee()
+    {
+        Vector3 playerPos = playerImage.transform.position;
+        for(float f = 0; f <= 1.0f; f += Time.deltaTime)
+        {
+            playerImage.transform.position -= new Vector3(500.0f * Time.deltaTime, 0, 0);
+            yield return null;
+        }
+
+
+        playerImage.transform.Rotate(0, -180, 0);
+        playerImage.transform.position = playerPos;
+        battleScreen.gameObject.SetActive(false);
+        yield break;
     }
 }
